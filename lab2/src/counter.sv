e4bf53b2f9727f3516473d4884005920c17e6bc9
jasonjiang200 <jasonjiang200@gmail.com>
2026-01-28 19:23:19 -0800

    Update comments and change to sv

--- lab2/src/counter.sv
+++ lab2/src/counter.sv
@@ -6,14 +6,14 @@ module counter (
     // TODO: delete this assignment once you write your own logic.
     assign LEDS = 4'd0;
 
-    // TODO: Instantiate a reg net to count the number of cycles
+    // TODO: Instantiate a register to count the number of cycles
     // required to reach one second. Note that our clock period is 8ns.
     // Think about how many bits are needed for your reg.
 
-    // TODO: Instantiate a reg net to hold the current count,
+    // TODO: Instantiate a register to hold the current count,
     // and assign this value to the LEDS.
 
-    // TODO: update the reg if clock is enabled (ce is 1).
+    // TODO: update the register if clock is enabled (ce is 1).
     // Once the requisite number of cycles is reached, increment the count.
 endmodule
 
